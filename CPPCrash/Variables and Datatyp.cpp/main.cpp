7277434d9db53747412c16e77009f05dd2a6f361
Anish Shinde <anishshinde55@gmail.com>
2026-06-29 18:00:28 +0530

    Variables and datatypes

---
+++ CPPCrash/Variables and Datatyp.cpp/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // //Variable define
+    // int age =19;
+    // cout<<"Anish age is:"<<age<<endl;
+    
+    //decalaration
+    int age;
+    cout<<age<<endl;
+
+    //definition
+    int age=19;
+    cout
+
+    return 0;
+
+}
