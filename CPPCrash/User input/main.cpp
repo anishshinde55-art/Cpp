96bf3893008701ee147095959658f467c524b969
Anish Shinde <anishshinde55@gmail.com>
2026-06-30 12:08:44 +0530

    User input

---
+++ CPPCrash/User input/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  //variable declare
+  bool marks;
+  cout<<"Enter your marks "<<endl;
+   
+  //input le rahe he
+  cin>>marks;
+  cout<<"Your marks:"<<marks<<endl;
+
+
+
+
+
+    return 0;
+
+}
