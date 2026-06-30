c7d32caa77ef7dc070249250701303f7e6aba904
Anish Shinde <anishshinde55@gmail.com>
2026-06-30 13:44:17 +0530

    Control Flows

---
+++ CPPCrash/Control Flows/main.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//    int budget;
+//    cout<<"Enter your budget:"<<endl;
+
+//    //input
+//    cin>>budget;
+
+//    if(budget>2000000){
+//     cout<<"You can buy scorpio"<<endl;
+//    }
+//    else {
+//     cout<<"You cannot buy scorpio"<<endl;
+//    }
+
+//  int marks;
+//  cout<<"Enter your marks:"<<endl;
+//  cin>>marks;
+//  cout<<"The marks are:"<<marks<<endl;
+//  if(marks>90){
+//     cout<<"A";
+//  }
+//  else if (marks>80){
+//     cout<<"B";
+
+//  }
+//  else if(marks>70){
+//     cout<<"C";
+
+//  }
+//  else if (marks>60){
+//     cout<<"D";
+
+//  }
+//  else if (marks>50)
+//  {
+//     cout<<"E";
+//  }
+//  else{
+//     cout<<"Fail"<<endl;
+//  }
+//     return 0;
+// }
+
+
+int height;
+cout<<"Enter height"<<endl;
+cin>>height;
+
+int weight;
+cout<<"Enter weight"<<endl;
+cin>>weight;
+
+
+if(height>5){
+    if(weight>70){
+        cout<<"You got a good bmi"<<endl;
+
+    }
+    else{
+        cout<<"Needs nutrituin"<<endl;
+
+    }
+
+}
+else{
+    cout<<"Protein le lo"<<endl;
+}
+
+
+
+return 0;
+
+}
