10d30e8bf05294feab657b2f7b3b2a4936606f77
Anish Shinde <anishshinde55@gmail.com>
2026-06-30 11:36:35 +0530

    Variables and Datatype

--- CPPCrash/Variables and Datatyp.cpp/main.cpp
+++ CPPCrash/Variables and Datatyp.cpp/main.cpp
@@ -6,13 +6,49 @@ int main(){
     // int age =19;
     // cout<<"Anish age is:"<<age<<endl;
     
-    //decalaration
-    int age;
-    cout<<age<<endl;
+    // //decalaration
+    // int age;
+    // cout<<age<<endl;
 
-    //definition
-    // int age=19;
-    // cout
+    // // //definition
+    //   int age=19;
+    //   cout<<age<<endl;
+
+    
+
+    // //manipulation otr updation
+    //    age = 101;
+    //  cout<<age<<endl;
+
+
+    // //int 
+    // int count =5;
+    // //float
+    // float share=3.14; 
+    // //char
+    // char alphabet='z';
+    // //double
+    // double weight =55.6895;
+    // //boolean
+    // bool isMale =true;
+    // bool ischild = 1;
+    // bool isfemale =0;
+
+    // cout<<count<<endl;
+    // cout<<share<<endl;
+    // cout<<alphabet<<endl;
+    // cout<<weight<<endl;
+    // cout<<isMale<<endl;
+
+    // age =12;
+    // cout<<sizeof(age)<<endl;
+ int age =12;
+ {
+    int age =3;
+
+
+ }
+ 
 
     return 0;
 
