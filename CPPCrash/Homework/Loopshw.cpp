f8b7ecdccf5707c5b2717b49aa1cb8f382824697
Anish Shinde <anishshinde55@gmail.com>
2026-07-01 17:13:19 +0530

    Loops

---
+++ CPPCrash/Homework/Loopshw.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // for(int i=1;i<=100;i++){
+    //     cout<<i<<endl;
+
+    // }
+
+    // for(int i=100;i>=1;i--){
+    //     cout<<i<<endl;
+    // }
+
+
+    // for(int i=1;i<=50;i++){
+    //     cout<<"Anish"<<endl;
+    // }
+
+    // for(int i=0;i>=-10;i--){
+    //     cout<<i<<endl;
+    // }
+   
+
+    // for(int i=1;i<=10;i++){
+    //     cout<<"The table of 7:"<<7*i<<endl;
+    // }
+
+    // for(char Ch='A';Ch<='Z';Ch++){
+    //     cout<<Ch<<endl;
+    // }
+    // for(char Ch='a';Ch<='z';Ch++){
+    //     cout<<Ch<<endl;
+    // }
+}
+
--- CPPCrash/Loops/loops.cpp
+++ CPPCrash/Loops/loops.cpp
@@ -6,7 +6,47 @@ int main(){
     for(int count=1;count<=10;count++){
         cout<<"Anish"<<endl;
     }
+    
+    //counting from 1 to 5
+    for (int i=1;i<=5;i++){
+        cout<<i<<endl;
+    }
 
+    //counting from 51 to 69
+    for (int i=51;i<=69;i++){
+        cout<<i<<endl;
+    }
+    for (int i=1;i<=10;i++){
+        cout<<i<<endl;
+        if(i==5){
+            break;
+        }
+       
+    }
+    for(int i=1;i<=10;i++){
+        if(i==5){
+            break;
+
+        }
+        cout<<"Iteration"<<i<<endl;
+    }     
+    for(int i=1;i<=5;i++){
+        if(i==4){
+            continue;
+
+        }
+        cout<<i<<endl;
+
+    } 
+
+
+    int i=1;
+    while(i<=5){
+        cout<<i<<endl;
+
+
+        i++;
+    }
 
 
 
