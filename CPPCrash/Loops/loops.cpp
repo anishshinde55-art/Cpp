9d20e7701380b01ff7812c51eaf4ae7cd763df1b
Anish Shinde <anishshinde55@gmail.com>
2026-07-01 14:00:31 +0530

    Loops

--- CPPCrash/Loops/loops.cpp
+++ CPPCrash/Loops/loops.cpp
@@ -1,1 +1,19 @@
 #include<iostream>
+using namespace std;
+
+int main(){
+    //for loop
+    for(int count=1;count<=10;count++){
+        cout<<"Anish"<<endl;
+    }
+
+
+
+
+
+
+    return 0;
+
+
+
+}
