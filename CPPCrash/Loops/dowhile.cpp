a98f7f02fb465ce85c08d2a3023617a542a39fe4
Anish Shinde <anishshinde55@gmail.com>
2026-07-02 14:01:36 +0530

    Loops

---
+++ CPPCrash/Loops/dowhile.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // for(int i=1;i<=5;i++){
+    //     cout<<i<<endl;
+
+    // }
+    // int i=1;
+    //   do {
+
+    //     //logic
+    //     cout<<i<<endl;
+    //     //updation
+    //     i++;
+        
+
+    // }while(i<=5);
+
+
+  // int count =20;
+  
+  // int i=1;
+  // do{
+  //   cout<<"Anish"<<endl;
+  //   i++;
+
+  // }while(i<=count);
+
+     
+  // int i;
+  // if (cout<<i){
+  //   cout<<"Anish";
+  // }
+
+
+  // for(int i=1;i<=3;i++){
+  //   for(int J=1;J<=3;J++){
+  //     cout<<"i:"<<i<<",J:"<<J<<endl;
+
+
+
+  //   }
+  // }
+   for(int i=1;i<=2;i++){
+    for(int j=1;j<=2;j++) {
+      cout<<i*j;
+    }
+   }
+
+
+
+
+    return 0;
+}
