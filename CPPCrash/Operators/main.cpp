6a841038b909d837fe02b1587bad56b6fe29596c
Anish Shinde <anishshinde55@gmail.com>
2026-07-04 12:36:35 +0530

    Operator

---
+++ CPPCrash/Operators/main.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    // int a=5;
+    // cout<<a<<endl;
+    // cout<<(++a)<<endl;
+    // // a ki value 6 ho chuki hai
+    // cout<<(a++)<<endl;
+    // //print kara toh 6 hi print hoga, but a ki value 7 ho chuki hogi
+    // cout<<a<<endl;
+    // //yaha tak a ki value 7 ban chuki hai
+    // cout<<(--a)<<endl;
+    // //a ki value 6 ban chuki hai
+    // cout<<(a--)<<endl;
+    // //print toh 6 hi hoga but a ki value 5 ban chuki h
+    // //lets verify
+    // cout<<a<<endl;
+
+
+
+    //Arithmetic operator
+    // int a=10;
+    // int b=5;
+
+    // cout<<a+b<<endl;
+    // cout<<(a-b)<<endl;
+    // cout<<(a*b)<<endl;
+    // cout<<(a/b)<<endl;
+    // cout<<(a%b)<<endl;
+
+
+    // cout<<5.0/2<<endl;
+
+
+    //Relational operators
+    // cout<<(10<5) <<endl;
+    // cout<<(10>5)<<endl;
+    // cout<<(10>=5)<<endl;
+    // cout<<(10<=5)<<endl;
+    // cout<<(10==5)<<endl;
+    // cout<<(10!=5)<<endl;
+
+
+
+  //Logical operators
+  //  bool cond1= 10>5;
+  //  bool cond2=5==5;
+  //  bool cond3= 2>=2;
+
+
+  //  if(cond1 || cond2 || cond3){
+  //   cout<<"Atleast one cond is true "<<endl;
+  //  }
+  //  else{
+  //   cout<<"All condition are not true";
+  //  }
+   
+    // bool condn = (5==10)  ;
+    // cout<<!condn<<endl;
+   
+
+   // Assignment operators
+
+  //  int arr =5;
+  //  cout<<arr<<endl;
+
+  //  //arr =arr+10;
+  //  arr+=10;
+  //  cout<<arr<<endl;
+
+  //  //arr=arr*10
+  //  arr*=10;
+  //  cout<<arr<<endl;
+   
+
+
+  // Bitwise operator
+  //  cout<<(5&4)<<endl;
+
+
+  //  cout<<(5|4)<<endl;  
+
+  //  cout<<(~5)<<endl;
+
+  //  cout<<(5<<2)<<endl;
+
+  //  cout<<(10>>1)<<endl;
+
+  //  cout<<(5^4)<<endl;
+
+
+   return 0;
+
+  }
+ 
