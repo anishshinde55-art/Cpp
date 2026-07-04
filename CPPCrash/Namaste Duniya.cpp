43a8600ebb9a592e352a509300a92097f06de1f9
Anish Shinde <anishshinde55@gmail.com>
2026-07-04 17:27:53 +0530

    Namaste duniya

--- CPPCrash/Namaste Duniya.cpp
+++ CPPCrash/Namaste Duniya.cpp
@@ -1,17 +1,6 @@
-// #include<iostream>
-// using namespace std;
-// int main() {
-//     cout<<"Namaste Dunia"<<endl;
-//     return 0;
-// }
-#include<iostream>
-using namespace std;
-int main(){
-    int a=5;
-    int b=7;
-    cout<<(a+b)<<endl;
-
-    return 0;
-
-
-}
+ #include<iostream>
+ using namespace std;
+ int main() {
+     cout<<"Namaste Dunia"<<endl;
+     return 0;
+ }
