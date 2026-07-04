c0c9ea605129e6e42f6b96325a30a250bbde254e
Anish Shinde <anishshinde55@gmail.com>
2026-07-04 17:26:21 +0530

    TypeCasting

--- CPPCrash/Namaste Duniya.cpp
+++ CPPCrash/Namaste Duniya.cpp
@@ -1,6 +1,17 @@
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     cout<<"Namaste Dunia"<<endl;
+//     return 0;
+// }
 #include<iostream>
 using namespace std;
-int main() {
-    cout<<"Namaste Dunia"<<endl;
+int main(){
+    int a=5;
+    int b=7;
+    cout<<(a+b)<<endl;
+
     return 0;
+
+
 }
