0f596c13e7f9390fb4f1d9c05252e39db99dbad5
Anish Shinde <anishshinde55@gmail.com>
2026-08-24 21:35:52 +0530

    Series and Patterns

---
+++ CPPCrash/Series&Pattern/CharacteeTrianglePattern.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    char ch='A';
+
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<i+1;j++){
+            
+            cout<<ch;
+            
+
+        }
+       cout<<endl;
+       ch++;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ CPPCrash/Series&Pattern/SquarePattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n =4;
+
+    for(int i=1;i<=n;i++){ //outer
+        for(int j=1;j<=n;j++){ //inner==>Line start
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
