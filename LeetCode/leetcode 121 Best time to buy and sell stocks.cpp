27c806b3d52ead1fcf3886652d995a1f646ba081
Anish Shinde <anishshinde55@gmail.com>
2026-08-29 21:31:50 +0530

    Pow(x,n) and Best time to buy or sell stocks

---
+++ LeetCode/leetcode 121 Best time to buy and sell stocks.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maximumprofit=0;
+        int bestbuy=prices[0];
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]>bestbuy){
+                maximumprofit=max(maximumprofit,prices[i]-bestbuy);
+
+            }
+            bestbuy=min(bestbuy,prices[i]);
+        }
+        return maximumprofit;
+    }
+};
